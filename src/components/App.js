d1ba5ed1a5f669b24d91e802d8cb831670d054a3
Oksana Plyas <plyasoksana2021@gmail.com>
2022-06-09 19:52:54 +0200

    fix hero mobile

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import Header from './Header';
 import Hero from "./Hero";
 import Footer from "./Footer";
+import VideoBlock from "./VideoBlock/VideoBlock";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
     <Header/>
     <main>
     <Hero />
+    <VideoBlock/>
     </main>
      
       <Footer />
---
+++ src/components/VideoBlock/VideoBlock.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Container from '../Container';
+
+const VideoBlock = () => {
+    return (<section>
+    <Container>
+    section
+    </Container>
+
+    </section>)
+};
+
+export default VideoBlock;
